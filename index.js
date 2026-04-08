02045e547ca3d8ee6f61fabc309d3113bdf61a17
Svyatoslav Savchuk <savchuksr@gmail.com>
2026-04-08 20:56:36 +0200

    newchange

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://sufitymontaz.pl', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+app.options('*', cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api/user', routerUser)
